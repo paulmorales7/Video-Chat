c646f2d92f9d5e9114f9df79569e28bade73dbcf
Paul Morales <paulginga@hotmail.com>
2021-03-22 18:58:42 -0500

    requesting access to camera in script.js

--- public/script.js
+++ public/script.js
@@ -1,3 +1,18 @@
+let videoStream
+const myVideo = document.createElement('video');
+myVideo.muted = true;
+
 navigator.mediaDevices.getUserMedia({
-    video
+    video: true,
+    audio: true
+}).then(stream => {
+videoStream = stream;
+addVideoStream(myVideo, stream)
 })
+
+const addVideoStream = (video, stream) => {
+    video.srcObject = stream;
+    video.addEventListener('loadedmetadata', () => {
+        video.play();
+    })
+}
