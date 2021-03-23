8148af46b9ff84d670d16f82b7684cf38042a6ef
Paul Morales <paulginga@hotmail.com>
2021-03-22 19:35:52 -0500

    added socket.io for real time connection

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
+const io = require('socket.io')(server)
 const { v4: uuidV4 } = require('uuid');
 
 app.set('view engine', 'ejs');
@@ -16,7 +17,11 @@ app.get('/:room', (req, res) => {
     res.render('room', { roomId: req.params.room })
 })
 
+io.on('connection', socket => {
+    socket.on('join-room', () => {
 
+    })
+})
 
 
 
